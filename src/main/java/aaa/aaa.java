aa084ac9b956089cb4c40c6782a01614cce60f20
Anna Szwajkowska <szwajkowska.a@gmail.com>
2018-02-24 20:59:33 +0100

    first commit

--- src/main/java/aaa/aaa.java
+++ src/main/java/aaa/aaa.java
@@ -1,8 +1,34 @@
 package aaa;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class aaa {
+
+
 
     public static void main(String[] args) {
         System.out.println("change");
+        copyTo();
     }
+
+    public static void copyTo(){
+        File newFile = new File("C:\\Users\\lukasz\\Desktop\\Ania\\X\\badName.txt");
+        File oldFile = new File("C:\\Users\\lukasz\\Desktop\\Ania\\Y\\properName.txt");
+        File dir = new File("C:\\Users\\lukasz\\Desktop\\Ania\\Y");
+        try {
+            if (oldFile.exists()){
+//                FileUtils.forceDelete(oldFile);
+                oldFile.delete();
+            }
+            FileUtils.copyFileToDirectory(newFile, dir);
+            newFile.renameTo(new File("C:\\Users\\lukasz\\Desktop\\Ania\\Y\\properName.txt"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
