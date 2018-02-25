2f6ba268bb72ece05a4ad8596b9cf0d89b87d133
Anna Szwajkowska <szwajkowska.a@gmail.com>
2018-02-25 20:53:25 +0100

    first change on newImage

--- src/main/java/aaa/aaa.java
+++ src/main/java/aaa/aaa.java
@@ -11,6 +11,7 @@ public class aaa {
 
     public static void main(String[] args) {
         System.out.println(" first change on master");
+        System.out.println("first change on newImage");
     }
 
     public static void copyTo(){
